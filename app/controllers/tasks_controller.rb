d9c8a0623cac8863caee04fa5cbcd40cd5023440
ryosuke takasawa <ten.r0619@gmail.com>
2020-04-18 10:09:58 +0900

    タスクの一覧画面、作成画面、詳細画面、編集画面、削除機能を作成した

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show,:edit,:update,:destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path,notice: "タスクが登録されました"
+    else
+      render "new"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path, notice: "タスクは更新されました"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to root_path,notice: "タスクは削除されました"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task_name,:description)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
