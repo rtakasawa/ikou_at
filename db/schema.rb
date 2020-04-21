ed6d524b474036cfbd02291ac6abbd3a0812716e
ryosuke takasawa <ten.r0619@gmail.com>
2020-04-21 22:20:18 +0900

    task_nameカラムにindexをつけた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_075354) do
+ActiveRecord::Schema.define(version: 2020_04_21_130125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_04_21_075354) do
     t.datetime "updated_at", null: false
     t.date "deadline", null: false
     t.string "status", null: false
+    t.index ["task_name"], name: "index_tasks_on_task_name"
   end
 
 end
