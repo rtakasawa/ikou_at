2d7bbb242b3251a9bbd2dd5725037817a093bd2f
ryosuke takasawa <ten.r0619@gmail.com>
2020-04-21 22:54:35 +0900

    検索ロジックをmodelへ移行

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ApplicationRecord
   validates :task_name, presence: true
   validates :description, presence: true
   validates :deadline, presence: true
+  scope :search_task_name, ->(task_name) {where("task_name LIKE ?", "%#{task_name}%")}
+  scope :search_status, ->(status) {where(status: status)}
 end
