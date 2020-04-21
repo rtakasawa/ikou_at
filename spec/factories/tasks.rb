37e1a565b56df95ab4d44f417a41874521def47a
ryosuke takasawa <ten.r0619@gmail.com>
2020-04-21 16:41:06 +0900

    終了期限に関するテストを実装した

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,15 +2,18 @@ FactoryBot.define do
   factory :task do
     task_name { 'test_name1' }
     description { 'test_description1' }
+    deadline { '1900-01-01' }
   end
 
   factory :second_task, class: Task do
     task_name { 'test_name2' }
     description { 'test_description2' }
+    deadline { '2000-01-01' }
   end
 
   factory :third_task, class: Task do
     task_name { 'test_name3' }
     description { 'test_description3' }
+    deadline { '2100-01-01' }
   end
 end
