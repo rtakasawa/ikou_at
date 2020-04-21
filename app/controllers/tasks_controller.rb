8a888a9f898be5ab63b72e5365afea26180540d4
ryosuke takasawa <ten.r0619@gmail.com>
2020-04-21 17:17:30 +0900

    tasksテーブルのカラムにステータスを追加した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:task_name, :description, :deadline)
+    params.require(:task).permit(:task_name, :description, :deadline, :status )
   end
 
   def set_task
