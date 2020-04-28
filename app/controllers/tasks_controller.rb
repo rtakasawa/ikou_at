92e2933e27094194a5155093d3fea15510e1b596
ryosuke takasawa <ten.r0619@gmail.com>
2020-04-28 17:32:46 +0900

    scopeでの検索機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,23 +3,48 @@ class TasksController < ApplicationController
   before_action :login_check_task
 
   def index
+    # 終了期限でソート
     if params[:sort_expired] == "true"
       @tasks = current_user.tasks.order(deadline: :desc).page(params[:page])
+      # 優先順位でソート
     elsif params[:sort_rank] == "true"
       @tasks = current_user.tasks.order(rank: :desc).page(params[:page])
+      #検索機能
     elsif params[:search].present?
-      if params[:search][:task_name].present? && params[:search][:status].present?
-        @tasks = current_user.tasks.search_task_name(params[:search][:task_name])
-                   .search_status(params[:search][:status])
-                   .page(params[:page])
-      elsif params[:search][:task_name].present?
-        @tasks = current_user.tasks.search_task_name(params[:search][:task_name]).page(params[:page])
-      else
-        @tasks = current_user.tasks.search_status(params[:search][:status]).page(params[:page])
-      end
+      @search_params = task_search_params
+      @tasks = current_user.tasks.search(@search_params).includes(:labels).page(params[:page])
     else
       @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(10)
     end
+
+    # # 終了期限でソート
+    # if params[:sort_expired] == "true"
+    #   @tasks = current_user.tasks.order(deadline: :desc).page(params[:page])
+    #   # 優先順位でソート
+    # elsif params[:sort_rank] == "true"
+    #   @tasks = current_user.tasks.order(rank: :desc).page(params[:page])
+    #   #検索機能
+    # elsif params[:search].present?
+    #   binding.irb
+    #   if params[:search][:task_name].present? && params[:search][:status].present?
+    #     @tasks = current_user.tasks.search_task_name(params[:search][:task_name])
+    #                .search_status(params[:search][:status])
+    #                .page(params[:page])
+    #     # タスク名の検索機能
+    #   elsif params[:search][:task_name].present?
+    #     @tasks = current_user.tasks.search_task_name(params[:search][:task_name]).page(params[:page])
+    #     # 優先順位の検索機能
+    #   elsif params[:search][:status].present?
+    #     @tasks = current_user.tasks.search_status(params[:search][:status]).page(params[:page])
+    #     # ラベルの検索機能
+    #   else
+    #     @task_id = TaskToLabel.search_label_id(params[:search][:label_id]).pluck(:task_id)
+    #     @tasks = current_user.tasks.search_task_to_label(@task_id).page(params[:page])
+    #   end
+    #   # 検索条件がない場合
+    # else
+    #   @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(10)
+    # end
   end
 
   def new
@@ -68,5 +93,9 @@ class TasksController < ApplicationController
     unless logged_in?
       redirect_to new_session_path
     end
+  end
+
+  def task_search_params
+    params.fetch(:search, {}).permit(:task_name, :status, :label_id)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -7,11 +7,45 @@ class Task < ApplicationRecord
   has_many :task_to_labels, dependent: :destroy
   has_many :labels, through: :task_to_labels
 
-  scope :search_task_name, ->(task_name) {where("task_name LIKE ?", "%#{task_name}%")}
-  scope :search_status, ->(status) {where(status: status)}
-
   enum rank:{ 低: 0, 中: 1, 高: 2 }
 
   paginates_per 10
+
+  scope :search, -> (search_params) do
+    return if search_params.blank?
+
+    search_task_name(search_params[:task_name])
+      .search_status(search_params[:status])
+      .search_label(search_params[:label_id])
+  end
+
+  scope :search_task_name, ->(task_name) {where("task_name LIKE ?", "%#{task_name}%") if task_name.present? }
+  scope :search_status, ->(status) {where(status: status) if status.present? }
+
+  scope :search_label, ->(label_id) do
+    if label_id.present?
+      task_id = TaskToLabel.all.search_label_id(label_id).pluck(:task_id)
+      where(id: task_id )
+    end
+  end
+
+#これをscopeで書きたい
+#   @task_id = TaskToLabel.where(label_id:params[:search][:label_id]).pluck(:task_id)
+#   @tasks = current_user.tasks.where(id: @task_id)
+
+
+  # scope :search_label, ->(label_id) {where(label_id: label_id) if label_id.present? }.pluck(:task_id)
+  # def search_label
+  #   @task_id = TaskToLabel.search_label_id(params[:search][:label_id]).pluck(:task_id)
+  #   @tasks = current_user.tasks.search_task_to_label(@task_id).page(params[:page])
+  # end
+
+  # scope :search_label, ->(label_id) {where(label_id: label_id) if label_id.present?}.pluck(task_id)
+  # scope :search_label, ->(label_id) {where(label_id: label_id) if label_id.present?}.pluck(task_id)
+  # scope :search_label, ->(task_id) {where(label_id: task_id)}
+
+  # scope :search_task_name, ->(task_name) {where("task_name LIKE ?", "%#{task_name}%")}
+  # scope :search_status, ->(status) {where(status: status)}
+  # scope :search_label_id, ->(label_id) {where(label_id: label_id)}
 
 end
